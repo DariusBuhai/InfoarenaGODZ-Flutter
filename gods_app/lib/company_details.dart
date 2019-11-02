66ca3991965c1fda7b16506a0cd783aec7408117
Buhai Darius <buhaidarius@gmail.com>
2019-11-03 00:54:22 +0200

    Update

--- gods_app/lib/company_details.dart
+++ gods_app/lib/company_details.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'stock_chart.dart';
+import 'slide_item.dart';
 
+
+List restaurants = [
+  {
+    "title": "Happy Jones",
+    "address": "1278 Loving Acres RoadKansas City, MO 64110",
+    "rating": "4.5"
+  }
+];
 
 class CompanyDetails extends StatefulWidget {
 
@@ -22,8 +31,32 @@ class _CompanyDetailsState extends State < CompanyDetails > {
       ),
       body: Column(
         children: < Widget > [
+          Text("\nStock Prices", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40)),
           Container(
             child: StockChart(widget._companyCode),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height/2.4,
+            width: MediaQuery.of(context).size.width,
+            child: ListView.builder(
+              primary: false,
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: restaurants == null ? 0 :restaurants.length,
+              itemBuilder: (BuildContext context, int index) {
+                Map restaurant = restaurants[index];
+
+                return Padding(
+                  padding: EdgeInsets.only(right: 10.0),
+                  child: SlideItem(
+                    img: " ",
+                    title: restaurant["title"],
+                    address: restaurant["address"],
+                    rating: restaurant["rating"],
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
