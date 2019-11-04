7efb9514c5811fef83e1eb068344404003781c42
Buhai Darius <buhaidarius@gmail.com>
2019-11-05 00:07:10 +0200

    Update

--- gods_app/lib/constants.dart
+++ gods_app/lib/constants.dart
@@ -1,9 +1,11 @@
-const String BASE_IP = "http://172.16.27.221:8000";
-const String BASE_IP_STOCKS = "http://172.16.27.221:8000/stocks/";
-const String BASE_IP_COMPANIES = "http://172.16.27.221:8000/companies/";
-const String BASE_IP_NEWS = "http://172.16.27.221:8000/classified_news/";
-const String BASE_IP_TWEETS = "http://172.16.27.221:8000/classified_tweets/";
-const String BASE_IP_FINAL_DATA = "http://172.16.27.221:8000/classified_stocks/";
-const String BASE_IP_HEADLINE = "http://172.16.27.221:8000/predict_headline_stocks/";
-
+const String IP = "192.168.0.101";
+const String PORT = ":8000";
+const String HTTP = "http://";
+const String BASE_IP = HTTP+IP+PORT;
+const String BASE_IP_STOCKS = BASE_IP+"/stocks/";
+const String BASE_IP_COMPANIES = BASE_IP+"/companies/";
+const String BASE_IP_NEWS = BASE_IP+"/classified_news/";
+const String BASE_IP_TWEETS = BASE_IP+"/classified_tweets/";
+const String BASE_IP_FINAL_DATA = BASE_IP+"/classified_stocks/";
+const String BASE_IP_HEADLINE = BASE_IP+"/predict_headline_stocks/";
 // ip constants used for http requests
